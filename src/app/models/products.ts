0121290c5d36d47379e35feb34b1285c5abab035
FabianaArciniegas <fabianaarciniegasc@gmail.com>
2024-12-06 11:38:52 -0500

    products

---
+++ src/app/models/products.ts
@@ -0,0 +1,10 @@
+export interface Products{
+    id_product: number;
+    name: string;
+    reference: string;
+    price: number;
+    stock: number;
+    brand: string;
+    size: string;
+    image_url: string;
+  }
